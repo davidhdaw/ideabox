c9bd803509c1b477416658c22f099bde7d218593
Kristy Nguyen <knyen008@gmail.com>
2022-04-12 16:24:58 -0600

    Create query selectors

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 var ideaList = []
+
+//querySelectors
+var showStarredIdeasButton = document.querySelector(.'show-starred-ideas');
+var titleInput = document.querySelector('.title-input');
+var bodyInput = document.querySelector('.body-input');
+var saveButton = document.querySelector('.save');
+var searchIdeas = document.querySelector('.search-ideas');
+var starEmpty = document.querySelector('.star-empty');
+var deleteCard = document.querySelector('.delete-card');
+var commentButton = document.querySelector('footer');
