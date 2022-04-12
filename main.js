47a363476e158f7bd12c843ae867ae9205832866
Kristy Nguyen <knyen008@gmail.com>
2022-04-12 17:15:07 -0600

    Create display user title and body function

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var searchIdeas = document.querySelector('.search-ideas');
 var starEmpty = document.querySelector('.star-empty');
 var deleteCard = document.querySelector('.delete-card');
 var commentButton = document.querySelector('footer');
+var cardGrid = document.querySelector('.cards-grid');
 
 //event listeners
 // titleInput.addEventListener('input', checkInput);
@@ -19,12 +20,31 @@ saveButton.addEventListener('click', addToIdeaList);
 //
 // }
 
+//Data model functions
 function addToIdeaList(event) {
   event.preventDefault();
   var userTitle = titleInput.value;
   var userBody = bodyInput.value;
   var newIdea = new Idea(userTitle, userBody);
   ideaList.push(newIdea);
+}
+
+//DOM functions
+function displayNewCard(idea) {
+  cardGrid.innerHTML += `<article class='cards'>
+    <header>
+      <img class='star-empty' src = "assets/star.svg" alt = "favoriting idea" height = "25px" width = "25px">
+      <img class='delete-card' src = "assets/delete.svg" alt = "delete favorite idea" height="25px" width="25px">
+    </header>
+    <section class = 'card-content'>
+      <h3>${idea.title}</h3>
+      <p>${idea.body}</p>
+    </section>
+    <footer>
+      <img src = "assets/comment.svg" alt = "add comment" height="25px" width="25px">
+      <span>Comment</span>
+    </footer>
+  </article>`
 }
 
 
@@ -38,10 +58,6 @@ function addToIdeaList(event) {
 
 
 
-
-
-
-//Display on DOM
 //Clear fields
 //Disable if nothing in fields
 //Create new card and display to cards grid
