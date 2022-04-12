ff39e13d35bd1c565c2b6462cb9d81f4dce1cf21
Kristy Nguyen <knyen008@gmail.com>
2022-04-12 16:53:21 -0600

    Create add to ideaList function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var ideaList = []
 
-//querySelectors
-var showStarredIdeasButton = document.querySelector(.'show-starred-ideas');
+//query selectors
+var showStarredIdeasButton = document.querySelector('.show-starred-ideas');
 var titleInput = document.querySelector('.title-input');
 var bodyInput = document.querySelector('.body-input');
 var saveButton = document.querySelector('.save');
@@ -9,3 +9,39 @@ var searchIdeas = document.querySelector('.search-ideas');
 var starEmpty = document.querySelector('.star-empty');
 var deleteCard = document.querySelector('.delete-card');
 var commentButton = document.querySelector('footer');
+
+//event listeners
+// titleInput.addEventListener('input', checkInput);
+// bodyInput.addEventListener('input', checkInput);
+saveButton.addEventListener('click', addToIdeaList);
+
+// function checkInput {
+//
+// }
+
+function addToIdeaList(event) {
+  event.preventDefault();
+  var userTitle = titleInput.value;
+  var userBody = bodyInput.value;
+  var newIdea = new Idea(userTitle, userBody);
+  ideaList.push(newIdea);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Display on DOM
+//Clear fields
+//Disable if nothing in fields
+//Create new card and display to cards grid
